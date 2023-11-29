e605e410a253c5f4790bae9a9067042c65fb1a0f
Jo Kroese <jo@jokroese.com>
2023-11-29 14:42:48 +0100

    add nav bar

---
+++ src/lib/NavBar.svelte
@@ -0,0 +1,114 @@
+<script>
+	import logo from '$lib/assets/queering-the-map-logo.png';
+	import infoButton from '$lib/assets/info.svg';
+	import addButton from '$lib/assets/add.svg';
+</script>
+
+<nav>
+	<button class="overlay-trigger overlay-trigger--info" id="info" aria-label="open info overlay">
+		<span class="overlay-trigger__title">INFO</span>
+		<img class="overlay-trigger__icon" src={infoButton} alt="info" />
+	</button>
+
+	<div id="logo">
+		<img src={logo} alt="" />
+	</div>
+
+	<button class="overlay-trigger overlay-trigger--add" id="add" aria-label="open add overlay">
+		<img class="overlay-trigger__icon" src={addButton} alt="add" />
+	</button>
+</nav>
+
+<style>
+	#logo {
+		display: inline-block;
+		position: absolute;
+		text-align: center;
+		top: 1em;
+		width: 100%;
+		margin: 0 auto;
+		pointer-events: none;
+		z-index: var(--logo-z-index);
+	}
+
+	#logo img {
+		width: 100px;
+	}
+
+	@media (min-width: 800px) {
+		#logo img {
+			width: 200px;
+		}
+	}
+
+	.overlay-trigger {
+		border: none;
+		background-color: transparent;
+		padding: 0;
+		top: 0;
+		font-size: 2.4em;
+		cursor: pointer;
+		font-weight: bold;
+		position: fixed;
+		z-index: var(--overlay-trigger-z-index);
+		color: var(--color-dark);
+	}
+
+	@media (min-width: 800px) {
+		.overlay-trigger {
+			top: 0.5em;
+		}
+	}
+
+	.overlay-trigger .overlay-trigger__icon {
+		display: block;
+		width: 40px;
+		height: auto;
+	}
+
+	@media (min-width: 800px) {
+		.overlay-trigger .overlay-trigger__icon {
+			top: 0.25em;
+			width: 50px;
+		}
+	}
+
+	.overlay-trigger.overlay-trigger--info {
+		left: 0.25em;
+		top: 0.25em;
+		text-shadow: 0px 0px 3px white;
+	}
+
+	@media (min-width: 800px) {
+		.overlay-trigger.overlay-trigger--info {
+			left: 0.5em;
+		}
+	}
+
+	.overlay-trigger.overlay-trigger--info .overlay-trigger__icon {
+		@media (min-width: 800px) {
+			display: none;
+		}
+	}
+
+	.overlay-trigger.overlay-trigger--info .overlay-trigger__title {
+		display: none;
+	}
+
+	@media (min-width: 800px) {
+		.overlay-trigger.overlay-trigger--info .overlay-trigger__title {
+			display: initial;
+		}
+	}
+
+	.overlay-trigger.overlay-trigger--add {
+		right: 0.25em;
+		top: 0.25em;
+	}
+
+	@media (min-width: 800px) {
+		.overlay-trigger.overlay-trigger--add {
+			right: 0.5em;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import Map from '$lib/Map.svelte';
+	import NavBar from '$lib/NavBar.svelte';
 </script>
 
+<NavBar></NavBar>
 <Map></Map>
 
 <style>
