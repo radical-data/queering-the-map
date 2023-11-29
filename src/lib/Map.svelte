3a857bf909bb175b2fa309f018f3b9210fe466b7
Jo Kroese <jo@jokroese.com>
2023-11-29 14:00:26 +0100

    add basic map

---
+++ src/lib/Map.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	import { Map } from 'maplibre-gl';
+
+	let map: Map | undefined;
+	let mapContainer: HTMLDivElement;
+
+	const maptilerApiKey = 'SRfJh1CuGiISgDoqUg55';
+	const maptilerMapReference = 'd27741ff-e220-4106-a5a1-aedace679204';
+	const initialState = { lng: -73.567256, lat: 45.501689, zoom: 13 };
+
+	onMount(() => {
+		map = new Map({
+			container: mapContainer,
+			style: `https://api.maptiler.com/maps/${maptilerMapReference}/style.json?key=${maptilerApiKey}`,
+			center: [initialState.lng, initialState.lat],
+			zoom: initialState.zoom
+		});
+	});
+
+	onDestroy(() => {
+		if (map) {
+			map.remove();
+			map = undefined;
+		}
+	});
+</script>
+
+<div id="map" bind:this={mapContainer}></div>
+
+<style>
+	@import 'maplibre-gl/dist/maplibre-gl.css';
+
+	#map {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Map from '$lib/Map.svelte';
+</script>
+
+<Map></Map>
+
+<style>
+	@import '$lib/style.css';
+</style>
