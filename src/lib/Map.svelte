f66d3e249c3a1228ad14854674adeaea6c967b15
Jo Kroese <jo@jokroese.com>
2023-11-30 16:32:44 +0100

    load and show data

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import { onMount, onDestroy } from 'svelte';
 	import { Map, NavigationControl } from 'maplibre-gl';
-	import 'maplibre-gl/dist/maplibre-gl.css'
+	import 'maplibre-gl/dist/maplibre-gl.css';
+
+	import moments from '$lib/data/filtered_data.json';
 
 	let map: Map | undefined;
 	let mapContainer: HTMLDivElement;
 
 	const maptilerApiKey = 'SRfJh1CuGiISgDoqUg55';
 	const maptilerMapReference = 'd27741ff-e220-4106-a5a1-aedace679204';
-	const initialState = { lng: -73.567256, lat: 45.501689, zoom: 13 };
+	const initialState = { lng: -73.567256, lat: 45.501689, zoom: 12.5 };
 
 	onMount(() => {
 		map = new Map({
@@ -21,6 +23,25 @@
 		});
 		map.addControl(new NavigationControl({ showCompass: false }), 'bottom-right');
 		map.keyboard.enable();
+
+		map.on('load', () => {
+			if (map) {
+				map.addSource('moments', {
+					type: 'geojson',
+					data: moments
+				});
+
+				map.addLayer({
+					id: 'moments-layer',
+					type: 'circle',
+					source: 'moments',
+					paint: {
+						'circle-radius': 8,
+						'circle-color': 'black'
+					}
+				});
+			}
+		});
 	});
 
 	onDestroy(() => {
