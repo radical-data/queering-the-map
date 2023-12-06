8806c2d364a4c58225f4e38936daa22f32455b2e
Jo Kroese <jo@jokroese.com>
2023-12-06 14:54:13 +0100

    specify rollup explicitly

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					initialDataChunk: [
+						path.resolve(__dirname, 'src/lib/data/filtered_data_id_only.json'),
+					],
+				}
+			}
+		}
 	}
 });
