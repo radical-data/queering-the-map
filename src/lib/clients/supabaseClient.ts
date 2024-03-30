56a4bf9f9118d07419126a25d09508b4fa7d7a2c
narcode <contact@felipeignacio.info>
2024-03-30 22:07:41 +0100

    add supabase JS client

---
+++ src/lib/clients/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+// import { Database } from './database.types'
+
+// export const supabase = createClient<Database>(
+//   process.env.SUPABASE_URL,
+//   process.env.SUPABASE_ANON_KEY
+// )
