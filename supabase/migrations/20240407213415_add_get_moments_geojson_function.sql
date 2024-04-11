8a237eec53ddeaa125ecf0da131470f825018f57
Jo Kroese <jo@jokroese.com>
2024-04-11 13:53:03 +0200

    Ensure get_moments_geojson always returns valid GeoJSON
    returns an empty geojson if the database is empty (or has no approved moments), avoiding potential unexpected behaviour.

--- supabase/migrations/20240407213415_add_get_moments_geojson_function.sql
+++ supabase/migrations/20240407213415_add_get_moments_geojson_function.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION public.get_moments_geojson()
 RETURNS JSON AS $$
 BEGIN
     RETURN (
-        SELECT json_build_object(
+        SELECT COALESCE(json_build_object(
             'type', 'FeatureCollection',
             'features', json_agg(
                 json_build_object(
@@ -11,7 +11,10 @@ BEGIN
                     'geometry', ST_AsGeoJSON(location)::json
                 )
             )
-        )
+        ), json_build_object(
+            'type', 'FeatureCollection',
+            'features', json_build_array()
+        ))
         FROM public.moments
         WHERE moments.status = 'approved'
     );
