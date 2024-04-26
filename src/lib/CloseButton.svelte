c86533564ca4fa59021259ee818ba679a92da576
rorotx <49108323+rorotx@users.noreply.github.com>
2024-04-26 12:01:31 -0300

    Finished - only logo change left

--- src/lib/CloseButton.svelte
+++ src/lib/CloseButton.svelte
@@ -4,6 +4,7 @@
 	export let functionOnClick: any;
 	export let position: 'right' | 'left' = 'left';
 	let closeButtonButton = closeButton;
+	let general_button__fadein = ''
 </script>
 
 <button
@@ -13,12 +14,7 @@
 	style={position === 'right' ? 'right: 0' : ''}
 >
 	<span class="close-btn__text">
-		<!-- svelte-ignore a11y-mouse-events-have-key-events -->
-		<img src={closeButtonButton} on:mouseover={()=>{
-			closeButtonButton = closeButtonHover;
-		}} on:mouseleave={()=>{
-			closeButtonButton = closeButton;
-		}} alt="close" />
+		<svg class="general_button__fadein" xmlns="http://www.w3.org/2000/svg" fill="#f4b9d6" viewBox="0 0 45 45"><rect x="0.5" y="0.5" width="44" height="44" fill="#f4b9d6" stroke="black"></rect><line x1="15.3536" y1="14.6464" x2="31.617" y2="30.9099" stroke="black"></line><line x1="14.6464" y1="30.9099" x2="30.9099" y2="14.6464" stroke="black"></line></svg>
 	</span>
 </button>
 
