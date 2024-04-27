5a01784a08c8474c8d24322402b271f5462d472d
Jo Kroese <jo@jokroese.com>
2024-04-27 17:12:32 +0300

    make GET function always return json
    Make error response return a structured JSON object with a 500 status code when an error occurs.

--- src/routes/moments/+server.ts
+++ src/routes/moments/+server.ts
@@ -1,12 +1,12 @@
 import { json } from '@sveltejs/kit';
-import {supabase}  from '$lib/clients/supabaseClient'
+import { supabase } from '$lib/clients/supabaseClient'
 
 export async function GET() {
     const { data, error } = await supabase.rpc('get_moments_geojson');
 
     if (error) {
         console.error('Error fetching moments:', error);
-        return;
+        return json({ error: 'Error fetching moments' }, { status: 500 });
     }
 
     return json(data);
