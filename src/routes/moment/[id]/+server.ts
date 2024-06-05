98d9bba4b2525b2fc2a324e154b76fb375e98cce
Jo Kroese <jo@jokroese.com>
2024-06-05 17:10:50 +0200

    get descriptions from file on svelte server

--- src/routes/moment/[id]/+server.ts
+++ src/routes/moment/[id]/+server.ts
@@ -1,13 +1,50 @@
 import { json, error } from '@sveltejs/kit';
-import { supabase } from '$lib/clients/supabaseClient'
+import fs from 'fs';
+import path from 'path';
+
+type DescriptionsCacheType = Record<number, string>;
+
+let descriptionsCache: DescriptionsCacheType | null = null;
+
+async function loadDescriptions(): Promise<DescriptionsCacheType | null> {
+    if (descriptionsCache) {
+        return descriptionsCache;
+    }
+
+    const descriptionsFilePath = path.resolve('static/data', 'descriptions.json');
+
+    return new Promise((resolve, reject) => {
+        fs.readFile(descriptionsFilePath, 'utf8', (err, fileContent) => {
+            if (err) {
+                console.error('Error reading descriptions file:', err);
+                reject(error(500, 'Error fetching moment text'));
+            } else {
+                descriptionsCache = JSON.parse(fileContent);
+                resolve(descriptionsCache);
+            }
+        });
+    });
+}
 
 export async function GET({ params }) {
     const { id } = params;
-    const { data, error: fetchError } = await supabase.from('moments').select('description').eq('short_id', parseInt(id, 10)).single()
 
-    if (fetchError) {
-        error(500, 'Error fetching moment text')
+    try {
+        const descriptions = await loadDescriptions();
+
+        if (!descriptions) {
+            throw error(500, 'Descriptions cache is empty');
+        }
+
+        const description = descriptions[Number(id)];
+
+        if (!description) {
+            throw error(404, 'Description not found');
+        }
+
+        return json({ short_id: id, description });
+    } catch (err) {
+        console.error('Error fetching moment text:', err);
+        throw error(500, 'Error fetching moment text');
     }
-
-    return json(data);
 }
