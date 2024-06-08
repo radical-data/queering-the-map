c9671ac312e687014a49245cabff8c7ee9ab6bec
Jo Kroese <jo@jokroese.com>
2024-06-08 15:02:30 +0200

    add marker sizes

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -15,6 +15,9 @@
 	const maptilerMapReference = 'd27741ff-e220-4106-a5a1-aedace679204';
 	const initialState = { lng: -73.567256, lat: 45.501689, zoom: 12.5 };
 
+	const markerHeight = 39;
+	const markerWidth = 10;
+	const markerCenter = 28;
 	const markerId = 'moments';
 	const markerLayerId = 'moments-layer';
 	const activeMarkerSourceId = 'active-marker-source';
