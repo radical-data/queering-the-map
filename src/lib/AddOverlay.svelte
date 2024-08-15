6b27e3218a0ca745a6c84505ff872f5f4c94783b
Narcode <contact@felipeignacio.info>
2024-08-15 23:47:57 +0200

    prevent submission of incomplete input and defer captcha load

--- src/lib/AddOverlay.svelte
+++ src/lib/AddOverlay.svelte
@@ -16,6 +16,7 @@
 
   let momentDescription = '';
   let captchaToken = '';
+  let isAddButtonDisabled = true;
 
   function closeAddOverlay() {
     addOverlayVisible.update(() => false);
@@ -38,6 +39,11 @@
       }
     );
   };
+
+  const verifyInputs = () => {
+    isAddButtonDisabled = !$activeMarkerCoords?.lng || !$activeMarkerCoords?.lat || !momentDescription
+  }
+
 
   async function handleAddMoment() {
     if (!captchaToken) {
@@ -116,12 +122,14 @@
               class="subform"
             ></textarea>
 
+            {#if !isAddButtonDisabled}
             <div
               style="margin-top: 16px"
               use:turnstile
               turnstile-sitekey={PUBLIC_CLOUDFLARE_TURNSTILE_SITE_KEY}
               on:turnstile={handleTurnstile}
             ></div>
+            {/if}
             <!-- <div class="recaptcha-text">
 							This site is protected by Turnstile and Cloudflare
 							<a href="https://www.cloudflare.com/privacypolicy/" target="_blank" rel="noopener"
@@ -149,7 +157,7 @@
                 rel="noopener">Privacy Policy</a
               >.
             </div>
-            <ActionButton functionOnClick={handleAddMoment}>Add</ActionButton>
+            <ActionButton functionOnClick={handleAddMoment} functionHover={verifyInputs} isDisabled={isAddButtonDisabled}>Add</ActionButton>
           </form>
         </div>
       </section>
